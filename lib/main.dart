8303a73ab4f6167c031075dc8722e442518782d4
quibolharvey <harveyquiboljr@gmail.com>
2024-10-17 09:18:11 +0800

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'YourName_MidtermExam',
+      title: 'Harvey_MidtermExam',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
